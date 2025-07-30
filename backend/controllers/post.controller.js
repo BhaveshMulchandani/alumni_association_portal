4dbd24d822e6d1f8213147836e9294d2113520a7
BhaveshMulchandani <bhaveshmulchandani330@gmail.com>
2025-07-30 19:33:54 +0530

    setup and sending files to imagekit account

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,10 +1,18 @@
-const postmodel = require('../models/postmodel')
+const {uploadfile} = require('../service/storage.service')
 
-const createpost = (req,res) => {
+const createpost = async (req,res) => {
     console.log(req.body);
     console.log(req.file)
 
+
+    const filedata = await uploadfile(req.file)
+    console.log(filedata);
+    
+
     res.status(200).json({message:"image created successfully"})
+
+
+
 }
 
 module.exports = {createpost}
